c869aca9d758e6c135f7abdeef089c6325c342b6
BoonHome88 <info.cynstudio@gmail.com>
2026-01-22 02:40:02 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,10 +1,11 @@
 // ตรวจสอบว่าผู้ใช้ล็อกอินหรือยัง
-if (!localStorage.getItem("loggedIn") && !window.location.pathname.endsWith("index.html")) {
-    window.location.href = "index.html"; // ถ้ายังไม่ล็อกอิน ให้กลับไปหน้า index.html
+if (!localStorage.getItem("loggedIn") && !window.location.pathname.endsWith("admin.html")) {
+    window.location.href = "admin.html"; // ถ้ายังไม่ล็อกอิน ให้กลับไปหน้า index.html
 }
 
 // ฟังก์ชัน Logout
 function logout() {
     localStorage.removeItem("loggedIn");
-    window.location.href = "index.html"; // กลับไปหน้า login
+    window.location.href = "admin.html"; // กลับไปหน้า login
 }
+
